1469465bdaae3a888872e929ccb8fb053ece3900
Daniel Kehoe <daniel@danielkehoe.com>
2012-10-12 16:54:45 -0700

    add authorization

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,10 +1,15 @@
 class ContentController < ApplicationController
+  before_filter :authenticate_user!
+  
   def silver
+    authorize! :view, :silver, :message => 'Access limited to Silver Plan subscribers.'
   end
-
+  
   def gold
+    authorize! :view, :gold, :message => 'Access limited to Gold Plan subscribers.'
   end
 
   def platinum
+    authorize! :view, :platinum, :message => 'Access limited to Platinum Plan subscribers.'
   end
 end
