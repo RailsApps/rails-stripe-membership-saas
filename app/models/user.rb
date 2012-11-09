5da7cba90cbdcbeb92d6bb5b97eb8d919154ef15
Daniel Kehoe <daniel@danielkehoe.com>
2012-11-09 09:43:00 -0800

    fix issue #14: make sure plan parameter persists after failed registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
   end
   
   def update_stripe
-    return if email.include?('@example.com') && (Rails.env.development? || Rails.env.test?)
+    return if email.include?('@example.com') and not Rails.env.production?
     if customer_id.nil?
       if !stripe_token.present?
         raise "Stripe token not present. Can't create account."
