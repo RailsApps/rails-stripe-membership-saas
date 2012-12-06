a50da9a543064aae5267928651e86fccd9d5d345
Brian Tse <briankftse@gmail.com>
2012-12-06 17:48:16 +0800

    Use resource.errors.full_messages instead of resource.errors[:base], as resource.errors[:base] returns an empty array.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   def display_base_errors resource
     return '' if resource.errors.empty?
-    messages = resource.errors[:base].map { |msg| content_tag(:p, msg) }.join
+    messages = resource.errors.full_messages.map { |msg| content_tag(:p, msg) }.join
     html = <<-HTML
     <div class="alert alert-error alert-block">
       <button type="button" class="close" data-dismiss="alert">&#215;</button>
