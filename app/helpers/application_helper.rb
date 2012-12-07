57ec699fc9e0013cde0330b10ef9d7972cddec94
Daniel Kehoe <daniel@danielkehoe.com>
2012-12-07 06:29:43 -0800

    close issue #17, display of empty e error div when form has errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def display_base_errors resource
-    return '' if resource.errors.empty?
+    return '' if (resource.errors.empty?) or (resource.errors[:base].empty?)
     messages = resource.errors[:base].map { |msg| content_tag(:p, msg) }.join
     html = <<-HTML
     <div class="alert alert-error alert-block">
