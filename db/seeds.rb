b632f8b155c5f3a12ab92d85fa9d16af09496e3f
Daniel Kehoe <daniel@danielkehoe.com>
2012-12-15 16:06:40 -0800

    use environment variables from application.yml file to set admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Environment variables (ENV['...']) are set in the file config/application.yml.
+# See http://railsapps.github.com/rails-environment-variables.html
 puts 'CREATING ROLES'
-Role.create([
-  { :name => 'admin' }, 
-  { :name => 'silver' }, 
-  { :name => 'gold' },
-  { :name => 'platinum' }
-], :without_protection => true)
+YAML.load(ENV['ROLES']).each do |role|
+  puts 'creating role: ' << role
+  Role.create({ :name => role }, :without_protection => true)
+end
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+user = User.create! :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+puts 'New user created: ' << user.name
 user.add_role :admin
-puts 'admin created: ' << user.name
 user2 = User.create! :name => 'Silver User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
 user2.add_role :silver
 user3 = User.create! :name => 'Gold User', :email => 'user3@example.com', :password => 'please', :password_confirmation => 'please'
