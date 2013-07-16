7fd78129142b5623b4f49bb485eb1cad29bbec3a
Vinh Quốc Nguyễn <kureikain@gmail.com>
2013-07-16 11:22:10 -0700

    Change hash notation when accessing card data.

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,7 +58,7 @@ class User < ActiveRecord::Base
       customer.description = name
       customer.save
     end
-    self.last_4_digits = customer.cards.data.first.last4    
+    self.last_4_digits = customer.cards.data.first["last4"]
     self.customer_id = customer.id
     self.stripe_token = nil
   rescue Stripe::StripeError => e
