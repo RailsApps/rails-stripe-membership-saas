ed9fd5b7edb74d79a1c98c1923e007b386a43493
Will Schive <lexigram@goodworksonearth.org>
2013-11-15 19:54:42 -0800

    fixed webhook to verify before action

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,10 +3,14 @@ STRIPE_PUBLIC_KEY = ENV["STRIPE_PUBLIC_KEY"]
 
 StripeEvent.setup do
   subscribe 'customer.subscription.deleted' do |event|
-    user = User.find_by_customer_id(event.data.object.customer)
-    user.expire
+    StripeEvent.event_retriever = lambda do |params|
+      verified_event = Stripe::Event.retrieve(params[event.id])
+          
+      user = User.find_by_customer_id(verified_event.data.object.customer)
+      user.expire
+    end
   end
-  
+
   subscribe 'customer.charge.succeeded' do |event|
     StripeEvent.event_retriever = lambda do |params|
       verified_event = Stripe::Event.retrieve(params[event.id])
