023da7ecbd6e010a7122f07daab4b8bbdb884405
Michael Bastos <bastosmichael@gmail.com>
2013-12-26 15:18:46 -0800

    fixed the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,25 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) are set in the file config/application.yml.
-# See http://railsapps.github.io/rails-environment-variables.html
+# See http://railsapps.github.com/rails-environment-variables.html
 puts 'ROLES'
 YAML.load(ENV['ROLES']).each do |role|
   Role.find_or_create_by_name({ :name => role }, :without_protection => true)
   puts 'role: ' << role
 end
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
+user = User.find_or_create_by_email :first_name => ENV['ADMIN_FIRST_NAME'].dup, :last_name => ENV['ADMIN_LAST_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+puts 'user: ' << user.first_name
 user.add_role :admin
-user2 = User.find_or_create_by_email :name => 'Silver User', :email => 'user2@example.com', :password => 'changeme', :password_confirmation => 'changeme'
-user2.add_role :silver
-user3 = User.find_or_create_by_email :name => 'Gold User', :email => 'user3@example.com', :password => 'changeme', :password_confirmation => 'changeme'
-user3.add_role :gold
-user4 = User.find_or_create_by_email :name => 'Platinum User', :email => 'user4@example.com', :password => 'changeme', :password_confirmation => 'changeme'
-user4.add_role :platinum
-puts "users: #{user2.name}, #{user3.name}, #{user4.name}"
+
+# begin 
+# 	require File.expand_path(File.dirname(__FILE__) + "/data/seed.rb")
+# rescue 
+	user2 = User.find_or_create_by_email :first_name => 'Silver', :last_name => 'User', :email => 'user2@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+	user2.add_role :silver
+	user3 = User.find_or_create_by_email :first_name => 'Gold', :last_name => 'User', :email => 'user3@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+	user3.add_role :gold
+	user4 = User.find_or_create_by_email :first_name => 'Platinum', :last_name => 'User', :email => 'user4@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+	user4.add_role :platinum
+	puts "users: #{user2.first_name}, #{user3.first_name}, #{user4.first_name}"
+# end
