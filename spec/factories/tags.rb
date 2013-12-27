22197eda1661a86af64b296eaa1ceb14ac1e0db4
Michael Bastos <bastosmichael@gmail.com>
2013-12-27 15:26:16 -0800

    Added a few more tests

---
+++ spec/factories/tags.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :tag do
+    name "MyString"
+    desc "MyString"
+    url "MyString"
+    image "MyString"
+    type ""
+  end
+end
