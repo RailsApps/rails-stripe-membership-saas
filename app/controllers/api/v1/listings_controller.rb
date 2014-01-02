f44411d4a8c1034eef42765d5d1997019ab99eb4
Michael Bastos <bastosmichael@gmail.com>
2014-01-02 10:17:06 -0800

    fixed unknowns and listings

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -18,6 +18,7 @@ module Api
         params.delete(:controller)
         params.delete(:access_token)
 
+
         if params[:url] && params[:name] && params[:image]
           listing = Listing.find_or_initialize_by_listing_id(:listing_id => params[:id],
                                                              :url => params[:url],
@@ -57,7 +58,7 @@ module Api
           # ListingWorker.perform_async(params)
         else
           # UnkownWorker.perform_async(params)
-          unknown = Unknown.find_or_initialize_by_unknown_id(:unknown_id => params[:id],
+          unknown = Unknown.find_or_initialize_by_listing_id(:listing_id => params[:id],
                                                              :url => params[:url],
                                                              :name => params[:name],
                                                              :image => params[:image],
@@ -70,24 +71,6 @@ module Api
           
           unknown.organization = Organization.find_by_name(params[:site_name])
           params.delete(:site_name)
-          
-          categories = params[:categories].split(',')
-          categories.each_with_index do |category, index|
-            ap index
-            c = Category.find_or_initialize_by_name(category)
-            c.subcategories << Category.find_or_create_by_name(categories[index+1]) rescue nil
-            unknown.taxonomies << c rescue nil
-            c.save
-          end
-          params.delete(:categories)
-
-          tags = params[:tags].split(',')
-          tags.each do |tag|
-            t = Tag.find_or_initialize_by_name(tag)
-            unknown.taxonomies << t rescue nil
-            t.save
-          end
-          params.delete(:tags)
           
           unknown.fields.merge!(params)
 
--- app/models/listings/unknown.rb
+++ app/models/listings/unknown.rb
@@ -1,3 +1,12 @@
-class Unknown < Listing
+class Unknown < Intangible
   # An unknown listing to sell an item—for example, an listing to sell a product, the DVD of a movie, or tickets to an event.
+  serialize :fields, ActiveRecord::Coders::Hstore
+  # has_and_belongs_to_many :taxonomies
+	
+  attr_accessible :listing_id,
+				  :product_id,
+				  :fields
+
+  belongs_to :item
+  belongs_to :organization
 end
