22822dcd608cae11a7b2292bd1104fb33e18b5c1
Michael Bastos <bastosmichael@gmail.com>
2014-01-03 23:26:19 -0800

    Updated Memberships and front page with top follows

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -3,13 +3,30 @@ class ContentController < ApplicationController
   
   def silver
     authorize! :view, :silver, :message => 'Access limited to Silver Plan subscribers.'
+    find_follows
   end
   
   def gold
     authorize! :view, :gold, :message => 'Access limited to Gold Plan subscribers.'
+    find_follows
   end
 
   def platinum
     authorize! :view, :platinum, :message => 'Access limited to Platinum Plan subscribers.'
+    find_follows
   end
+
+  private
+
+  def find_follows
+    listings = []
+    @listings = []
+    current_user.follows.each do |f|
+      if f.followable_type == "Intangible" then listings << f.followable_id end 
+    end
+    listings.each do |l|
+      @listings << Listing.find(l)
+    end
+  end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,27 @@
 class HomeController < ApplicationController
+
+	def index
+		find_top
+	end
+
+	private
+
+	def find_top
+		listings = []
+		@listings = []
+		Follow.all.each do |f|
+			if f.followable_type == "Intangible" then listings << f.followable_id end 
+		end
+		listings = find_top_5 listings
+		listings.each do |l|
+			@listings << Listing.find(l)
+		end
+	end
+
+	def find_top_5 list
+		count = Hash.new(0)
+		list.each {|element| count[element] += 1}
+		list = list.uniq.sort {|x,y| count[y] <=> count[x]}
+		list[0...6]
+	end
 end
