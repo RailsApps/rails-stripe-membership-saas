8cfa1cccc0bd5df4719b7ac068f96a9e13f2f402
Will Schive <lexigram@goodworksonearth.org>
2014-01-22 16:41:29 -0800

    removed unneeded file

--- app/assets/javascripts/bootstrap_three.js
+++
@@ -1,8 +0,0 @@
-// three modal windows
-// backdrop="true" shuts modal when outside modal area is clicked 
-// keyboard="true" shuts modal when escape key is pressed
-//$(document).ready(  function() {
-$('#changeplan').modal(data-backdrop="true" data-keyboard="true");
-$('#changecard').modal(data-backdrop="true" data-keyboard="true");
-$('#changerole').modal(data-backdrop="true" data-keyboard="true");
-//});
