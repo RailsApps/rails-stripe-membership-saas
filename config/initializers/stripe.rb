ac17a68d04315151400601580f2265bd8241dde3
Will Schive <lexigram@goodworksonearth.org>
2014-02-15 20:40:48 -0800

    stripe update, per note at update

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,8 @@
 Stripe.api_key = ENV["STRIPE_API_KEY"]
 STRIPE_PUBLIC_KEY = ENV["STRIPE_PUBLIC_KEY"]
 
-StripeEvent.setup do
+# changed from setup to configure : https://github.com/RailsApps/rails-stripe-membership-saas/issues/96#issuecomment-35108722
+StripeEvent.configure do
   subscribe 'customer.subscription.deleted' do |event|
     StripeEvent.event_retriever = lambda do |params|
       verified_event = Stripe::Event.retrieve(params[event.id])
