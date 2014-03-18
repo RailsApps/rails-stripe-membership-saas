be2f35bbff5ecb295ce80383247d61a6eebc325e
Will Schive <lexigram@goodworksonearth.org>
2014-03-17 17:25:44 -0700

    upgraded a find method to Rails 4

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,18 @@
 Stripe.api_key = ENV["STRIPE_API_KEY"]
 STRIPE_PUBLIC_KEY = ENV["STRIPE_PUBLIC_KEY"]
 
-# changed from setup to configure : https://github.com/RailsApps/rails-stripe-membership-saas/issues/96#issuecomment-35108722
+# Note: do not listen for customer.subscription.updated
+# Reference : http://imzank.com/2012/11/how-to-use-stripe-com-to-email-your-customers/
+
+# Changed StripeEvent#method from setup to configure
+# Reference : https://github.com/RailsApps/rails-stripe-membership-saas/issues/96#issuecomment-35108722
 StripeEvent.configure do
   subscribe 'customer.subscription.deleted' do |event|
     StripeEvent.event_retriever = lambda do |params|
       verified_event = Stripe::Event.retrieve(params[event.id])
           
-      user = User.find_by_customer_id(verified_event.data.object.customer)
+     #user = User.find_by_customer_id(verified_event.data.object.customer)
+     user = User.where(customer_id: :verified_event.data.object.customer)
       user.expire
     end
   end
