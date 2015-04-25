e0e6dddd8b768edb39a4f7141164822b85b6a86f
Will Schive <lexigram@goodworksonearth.org>
2015-04-24 17:00:37 -0700

    morer configurations in place, 3 RSpec tests pass

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => [:show] if @email
   before_action :identify_product
 
   def show
