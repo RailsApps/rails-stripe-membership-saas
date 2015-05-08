f162369f902d5a67087860ae5da0a47915e63310
Will Schive <lexigram@goodworksonearth.org>
2015-05-08 12:49:53 -0700

    visitor tests passing

--- spec/features/visitors/visiting_spec.rb
+++ spec/features/visitors/visiting_spec.rb
@@ -3,37 +3,48 @@ Warden.test_mode!
 
 RSpec.describe 'Visitor visits and sees site entrance points' do
 
-  let(:stripe_helper) { StripeMock.create_test_helper }
+  before(:each) do
+    visit root_path
+  end
 
   after(:each) do
     Warden.test_reset!
   end
 
   it 'allows visitor to arrive and see the Sign Up link' do
-    visit '/'
     expect(current_path).to eq root_path
     expect(page).to have_content 'Sign up'
-     expect(page).to have_link 'Sign up'
+    expect(page).to have_link 'Sign up'
   end
     
-  it 'allows visitor to see the Subscribe buttons' do
-    visit '/'
+  it 'allows visitor to see the first Subscribe button' do
     expect(page).to have_content 'Subscribe'
     expect(page).to have_link 'Subscribe'
   end
 
-  it 'does not allow visitor to see the product page' do
-    visit '/'
+  it 'does not allow visitor to see the /products available' do
     visit '/products/product.pdf'
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
-  #  expect(page).to have_link 'Subscribe'
   end
 
-  it 'does not allow visitor to see the protected content pages' do
-    visit '/'
-    visit 'content/silver'
+  it 'does not allow visitor to see the /content/silver page' do
+    visit '/content/silver'
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
-#    expect(page).to have_link 'Subscribe'
+  end
+
+  it 'does not allow visitor to see the /content/gold page' do
+    visit '/content/gold'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+
+  it 'does not allow visitor to see the /content/platinum page' do
+    visit '/content/platinum'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+
+  it 'does not allow visitor to see the /users page' do
+    visit '/users'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 
 end
