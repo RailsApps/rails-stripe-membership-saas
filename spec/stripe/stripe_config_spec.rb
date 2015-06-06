fd48dcd10c10edf625912b9f6cac625e17b673b9
kathyonu <lexigram@goodworksonearth.org>
2015-06-06 08:01:07 -0700

    Stripe Keys env variables tests

--- spec/stripe/stripe_config_spec.rb
+++ spec/stripe/stripe_config_spec.rb
@@ -1,18 +1,17 @@
-describe "Stripe Config Variables" do
+describe "Config Variables" do
 
   describe "STRIPE_API_KEY" do
-    it "a STRIPE_API_KEY is set in env" do
-      expect(ENV['STRIPE_API_KEY']).to eq(ENV.fetch("STRIPE_API_KEY")),
-        "Your STRIPE_API_KEY is not set, 
-         Please refer to the 'Configure the Stripe Initializer' section of the README"
+    # this tests env variables against config/secrets.yml which reads from your env variables $ `env`
+    it "a STRIPE_API_KEY is set" do
+      expect(ENV['STRIPE_API_KEY']).to eq(Rails.application.secrets.stripe_api_key),
+        "Your STRIPE_API_KEY is not set, Please refer to the 'Configure the Stripe Initializer' section of the README"
     end
   end
 
   describe "STRIPE_PUBLISHABLE_KEY" do
-    it "a STRIPE_PUBLISHABLE_KEY is set in env" do
-      expect(ENV['STRIPE_PUBLISHABLE_KEY']).to eq(ENV.fetch("STRIPE_PUBLISHABLE_KEY")),
-        "Your STRIPE_PUBLISHABLE_KEY is not set, 
-         Please refer to the 'Configure the Stripe Initializer' section of the README"
+    it "a STRIPE_PUBLISHING_KEY is set" do
+      expect(ENV['STRIPE_PUBLISHABLE_KEY']).to eq(Rails.application.secrets.stripe_publishable_key),
+        "Your STRIPE_PUBLISHABLE_KEY is not set, Please refer to the 'Configure the Stripe Initializer' section of the README"
     end
   end
 
